885b6817d7501f45b48f9dd86ab8ae362703fc03
Paciik <paciik@paciik.eu>
2015-01-12 15:47:40 +0100

    Text View

--- app/src/main/java/eu/paciik/gittest/MainActivity.java
+++ app/src/main/java/eu/paciik/gittest/MainActivity.java
@@ -4,9 +4,12 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
+
+    TextView text_view = (TextView) findViewById(R.id.text_view);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
